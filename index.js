017ade76d90ec89b004b8eff824ab436401578ef
Natan Simoes <natan16simoes@gmail.com>
2024-04-29 15:09:03 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.lenght = 0;
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length++;
+  }
 
-  max() {}
+  get(pos) {
+    if(pos > this.items.length - 1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+  }
 
-  sum() {}
+  return Math.max(...this.items);
+}
 
-  avg() {}
+  min() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+  return Math.min(...this.items);
+}
+
+sum() {
+  if(this.items.length === 0) {
+    return 0;
+  }
+  let sum = 0
+  this.items.forEach((number) => {
+    sum += number;
+  })
+  return sum;
+}
+
+avg() {
+  if(this.items.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  let sum = 0;
+  this.items.forEach((number) => {
+    sum += number;
+  })
+  return sum / this.items.length;
+}
 }
 
 module.exports = SortedList;
